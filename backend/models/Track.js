e8db22071bc777f4d589230f65ab6d37ecbd7ef0
Vitaliy Ten <vitalten10@gmail.com>
2021-11-27 13:14:46 +0600

    #89 добавлена поле артиста для вывода на экран исполнителя

--- backend/models/Track.js
+++ backend/models/Track.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose');
 const idValidator = require('mongoose-id-validator');
 
 const TrackSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  lasting: String,
-  album: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Album',
-    required: true
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    lasting: String,
+    album: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Album',
+        required: true
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Artist',
+        required: true
+    }
+
 });
 
 TrackSchema.plugin(idValidator);
